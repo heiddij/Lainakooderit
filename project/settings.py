06d17ee01ab8dd905ae74cfc052b783013c1f738
heiddij <heidi20151@student.hamk.fi>
2022-11-11 20:43:04 +0200

    Korjailuja tehty

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vqq0@w65m)8+csjs*am3e0hye1kf_mmrscnbsc^6k!s^*n-0wo'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.13.111.46']
+ALLOWED_HOSTS = ['51.13.111.46', 'localhost']
 
 
 # Application definition
